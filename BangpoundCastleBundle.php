35e2a6d3fd96d9a2b178f463e7016a4364f741d3
Benjamin Doherty <bjd@bangpound.org>
2014-07-19 15:15:43 -0500

    remove sonata notification bundle.

--- BangpoundCastleBundle.php
+++ BangpoundCastleBundle.php
@@ -14,9 +14,4 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class BangpoundCastleBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
-    {
-//        $container->addCompilerPass(new AddCouchDBTypesCompilerPass(), PassConfig::TYPE_BEFORE_OPTIMIZATION);
-//        $container->addCompilerPass(new NotificationAdminCompilerPass());
-    }
 }
