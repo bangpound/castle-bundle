4f04b01d73af89f1c14ec4e152891ce7322a5cf0
Benjamin Doherty <bjd@bangpound.org>
2014-11-02 11:15:29 -0600

    simplify link views.

--- Resources/couchdb/nofollow/views/timeseries/map.js
+++ Resources/couchdb/nofollow/views/timeseries/map.js
@@ -5,7 +5,9 @@ function (doc)
     var date = new Date(Date.UTC.apply(null, utc));
     if (date && doc.links) {
         doc.links.forEach(function (link) {
-            emit([link.rel, link.href, date.getUTCFullYear(), date.getUTCMonth() + 1, date.getUTCDate(), date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds()], 1);
+            if (link.rel === 'nofollow') {
+                emit([link.href, date.getUTCFullYear(), date.getUTCMonth() + 1, date.getUTCDate(), date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds()], 1);
+            }
         });
     }
 }
