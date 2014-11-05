4311f2c12e2895c4b153e85b681fb40c4dd19a81
Benjamin Doherty <bjd@bangpound.org>
2014-11-05 07:03:09 -0600

    remove collection view.

--- Resources/couchdb/collection/views/timeseries/map.js
+++
@@ -1,9 +0,0 @@
-function (doc)
-{
-    var utc = doc.published.split(/\D/);
-    utc[1] = utc[1] - 1;
-    var date = new Date(Date.UTC.apply(null, utc));
-    if (date) {
-        emit([ doc.type ], 1);
-    }
-}
